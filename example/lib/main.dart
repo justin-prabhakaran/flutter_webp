70d71780950b0a3480f42fe76d9bf7033db4911d
justin-prabhakaran <c.m.justinprabhakaran@gmail.com>
2025-08-31 23:05:52 +0530

    webp dynamic lib added for linux
    added simple getinfo c api
    
    improved

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
 import 'package:flutter/services.dart';
 import 'package:flutter_webp/flutter_webp.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  print("hello world");
+  final webp = FlutterWebp();
+  webp.hello();
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -16,48 +18,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _flutterWebpPlugin = FlutterWebp();
-
   @override
   void initState() {
+    () async {
+      //testing
+      final webp = FlutterWebp();
+
+      final data = await rootBundle.load("assets/test.jpg");
+
+      final bytes = data.buffer.asUint8List();
+
+      final res = webp.getInfo(bytes);
+      if (res.ok) {
+        print(res.width);
+        print(res.height);
+      }
+    }();
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _flutterWebpPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
--- lib/flutter_webp.dart
+++ lib/flutter_webp.dart
@@ -1,8 +1,54 @@
+import 'dart:ffi';
+import 'dart:typed_data';
 
-import 'flutter_webp_platform_interface.dart';
+import 'package:ffi/ffi.dart';
+
+typedef _WebPGetInfoC = Int32 Function(
+    Pointer<Uint8>, IntPtr size, Pointer<Int32>, Pointer<Int32>);
+typedef _WebPGetInfoDart = int Function(
+    Pointer<Uint8>, int, Pointer<Int32>, Pointer<Int32>);
 
 class FlutterWebp {
-  Future<String?> getPlatformVersion() {
-    return FlutterWebpPlatform.instance.getPlatformVersion();
+  late final DynamicLibrary _lib;
+  late final _WebPGetInfoDart _getInfo;
+
+  FlutterWebp() {
+    _lib = DynamicLibrary.open("../linux/libs/libwebp.so");
+    _getInfo =
+        _lib.lookupFunction<_WebPGetInfoC, _WebPGetInfoDart>("WebPGetInfo");
   }
+
+  WebPInfo getInfo(Uint8List data) {
+    final size = data.length;
+    final dataPrt = malloc<Uint8>(size);
+
+    final widthPtr = malloc<Int32>();
+    final heightPtr = malloc<Int32>();
+
+    final res = _getInfo(dataPrt, size, widthPtr, heightPtr);
+    print("result : $res");
+    print("Width: ${widthPtr.value}  Height: ${heightPtr.value}");
+
+    if (res != 0) {
+      return WebPInfo(width: widthPtr.value, height: heightPtr.value, ok: true);
+    }
+
+    return WebPInfo(height: -1, ok: false, width: -1);
+  }
+
+  void hello() {
+    print("hellow wordld");
+  }
+}
+
+class WebPInfo {
+  final int width;
+  final int height;
+  final bool ok;
+
+  const WebPInfo({
+    required this.width,
+    required this.height,
+    required this.ok,
+  });
 }
